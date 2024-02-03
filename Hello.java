042257d21006672afc8f0d15f2453783f951c889
sahilpahuja73 <sahilpahuja73@gmail.com>
2024-02-03 16:52:18 +0530

    new change

--- Hello.java
+++ Hello.java
@@ -1,11 +1,11 @@
-package seleenium;
+	cd package seleenium;
 
 public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println( "Hello world " );
+		System.out.println( "Hello" );
 	}
 
 }
